9da3040568d2f9216da39f01eb642ff1d7003e5a
Kiril Knysh <k.knysh@gmail.com>
2021-09-05 13:06:38 +0200

    Feature/new puzzle js (#4)
    * Add New Puzzles components;
    
    * Wrap querySelectorAll with try-catch
    
    * Add TODO comments;
    
    * Add NewJs component; Validate inputs;
    
    * Fix apple touch icon relative path;
    
    * Pass missing param;
    
    * Unify new css puzzle validator;
    
    * Highlight prop names;

--- src/components/puzzles/NewCss.js
+++ src/components/puzzles/NewCss.js
@@ -1,89 +1,223 @@
 // YOLO: on
 
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
 
 import style from './NewCss.css';
 
-// TODO:
-// self-closing tags, e.g. <br></br> === <br /><br />
-// validation (banned char, solution+input change)
-// expected = solution(input)  -->> move to state as `expected`
-
 export default function NewCss({ state, onChange }) {
+  const [lines, setLines] = useState([]);
+
   function onNameChange(event) {
-    onChange({ ...state, name: event.target.value });
+    const { value } = event.target;
+
+    if (!value) {
+      return onChange({ ...state, name: { value, valid: false, message: 'Name can not be empty' } });
+    }
+
+    onChange({ ...state, name: { value, valid: true } });
+  }
+
+  function onDescriptionChange(event) {
+    onChange({ ...state, description: { value: event.target.value, valid: true } });
   }
 
   function onBannedChange(event) {
     const { value } = event.target;
-    try {
-      const banned = JSON.parse(value);
-      onChange({ ...state, banned });
-    } catch {
-      console.log('Failed to parse `Banned characters`');
-    }
+
+    const { lines, expected, inputError, solutionError, bannedError } = validateSolution(state?.input?.value, state?.solution?.value, value);
+
+    setLines(lines);
+    onChange({
+      ...state,
+      expected,
+      banned: { value, valid: !bannedError, message: bannedError },
+      input: { ...state?.input, valid: !inputError, message: inputError },
+      solution: { ...state?.solution, valid: !solutionError, message: solutionError },
+    });
   }
 
   function onSolutionChange(event) {
-    onChange({ ...state, solution: event.target.value });
+    const { value } = event.target;
+
+    const { lines, expected, inputError, solutionError, bannedError } = validateSolution(state?.input?.value, value, state?.banned?.value);
+
+    setLines(lines);
+    onChange({
+      ...state,
+      expected,
+      banned: { ...state?.banned, valid: !bannedError, message: bannedError },
+      input: { ...state?.input, valid: !inputError, message: inputError },
+      solution: { value, valid: !solutionError, message: solutionError },
+    });
   }
 
   function onInputChange(event) {
-    onChange({ ...state, input: event.target.value });
+    const { value } = event.target;
+
+    const { lines, expected, inputError, solutionError, bannedError } = validateSolution(value, state?.solution?.value, state?.banned?.value);
+
+    setLines(lines);
+    onChange({
+      ...state,
+      expected,
+      banned: { ...state?.banned, valid: !bannedError, message: bannedError },
+      input: { value, valid: !inputError, message: inputError },
+      solution: { ...state?.solution, valid: !solutionError, message: solutionError },
+    });
   }
 
   return (
-    <div>
-      <div class={style.name}>
-        <div>Name:</div>
-        <input value={state?.name || ''} onInput={onNameChange} />
+    <div class={style.panels}>
+      <div class={style.inputs}>
+        <div class={style.title}>Puzzle data</div>
+        <div class={style.name}>
+          <div class={style.propName}>Name:</div>
+          <input class={state?.name?.valid ? style.inputValid : style.inputInvalid} value={state?.name?.value || ''} onInput={onNameChange} />
+          {state?.name?.message && <div class={style.errorMessage}>{state?.name?.message}</div>}
+        </div>
+        <div class={style.description}>
+          <div class={style.propName}>Description:</div>
+          <input value={state?.description?.value || ''} onInput={onDescriptionChange} />
+        </div>
+        <div class={style.banned}>
+          <div><span class={style.propName}>Banned characters</span> (JSON array):</div>
+          <input class={state?.banned?.valid ? style.inputValid : style.inputInvalid} value={state?.banned?.value || ''} onInput={onBannedChange} placeholder={'[",", "+", "d"]'} />
+          {state?.banned?.message && <div class={style.errorMessage}>{state?.banned?.message}</div>}
+        </div>
+        <div class={style.solution}>
+          <div class={style.propName}>Solution:</div>
+          <input class={state?.solution?.valid ? style.inputValid : style.inputInvalid} value={state?.solution?.value || ''} onInput={onSolutionChange} />
+          {state?.solution?.message && <div class={style.errorMessage}>{state?.solution?.message}</div>}
+        </div>
+        <div class={style.input}>
+          <div class={style.propName}>Input:</div>
+          <textarea class={state?.input?.valid ? style.inputValid : style.inputInvalid} value={state?.input?.value || ''} onInput={onInputChange} rows="5" cols="50" />
+          {state?.input?.message && <div class={style.errorMessage}>{state?.input?.message}</div>}
+          <MarkupRenderer lines={lines} expected={state?.expected} />
+        </div>
       </div>
-      <div class={style.banned}>
-        <div>Banned characters (JSON array):</div>
-        <input value={JSON.stringify(state?.banned || [])} onInput={onBannedChange} placeholder={'[",", "+", "d"]'} />
-      </div>
-      <div class={style.solution}>
-        <div>Solution:</div>
-        <input value={state?.solution || ''} onInput={onSolutionChange} />
-      </div>
-      <div class={style.input}>
-        <div>Input:</div>
-        <textarea value={state?.input || ''} onInput={onInputChange} rows="5" cols="50" />
-        <MarkupRenderer input={state?.input || ''} solution={state?.solution || ''} />
+      <div class={style.examples}>
+      <div class={style.title}>Example</div>
+        <div class={style.name}>
+          <div class={style.propName}>Name:</div>
+          <input value="Puzzle name" readonly="readonly" />
+        </div>
+        <div class={style.description}>
+          <div class={style.propName}>Description:</div>
+          <input value="Short description" readonly="readonly" />
+        </div>
+        <div class={style.banned}>
+          <div class={style.propName}>Banned characters:</div>
+          <input value={'["#", ","]'} readonly="readonly" />
+        </div>
+        <div class={style.solution}>
+          <div class={style.propName}>Solution:</div>
+          <input value=".spcl" readonly="readonly" />
+        </div>
+        <div class={style.input}>
+          <div class={style.propName}>Input:</div>
+          <textarea value={'<div><span class="spcl">content</span></div>'} readonly="readonly" rows="5" cols="50" />
+        </div>
       </div>
     </div>
   );
 }
 
-function MarkupRenderer({ input, solution }) {
-  const container = document.createElement('div');
-  container.innerHTML = input;
+function validateSolution(inputValue, solutionValue, bannedValue) {
+  let { input, inputError } = parseInput(inputValue);
+  let { solution, solutionError } = parseSolution(solutionValue);
+  let { banned, bannedError } = parseBanned(bannedValue);
+  let lines = [];
+  let expected = [];
 
-  const lines = nodesToLines(Array.from(container.children));
-
-  let selected = [];
-  if (solution) {
-    try {
-      selected = Array.from(container.querySelectorAll(solution)).map((node) => node.dataset['qdid']);
-    } catch (error) {
-      console.log('Failed to run solution', error);
+  // check solution for banned characters
+  if (!solutionError && !bannedError && banned.length > 0) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+    const escapeSymbolsRegex = /[.*+?^${}()|[\]\\]/g;
+    const chars = banned.map((char) => char.replace(escapeSymbolsRegex, '\\$&')).join('|');
+    if (new RegExp(`(${chars})`, 'i').test(solution)) {
+      solutionError = 'Solution can not contain banned characters';
     }
   }
 
+  // parse input to lines
+  if (!inputError) {
+    const container = document.createElement('div');
+    container.innerHTML = input;
+    lines = nodesToLines(Array.from(container.childNodes), { qdIdCounter: 0 }, 0);
+
+    // apply solution
+    if (!solutionError) {
+      try {
+        expected = Array.from(container.querySelectorAll(solution)).map((node) => node.dataset['qdid']);
+        if (expected.length < 1) {
+          solutionError = 'Given solution does not query any node from the input';
+        }
+      } catch (error) {
+        console.log('Failed to run solution', error);
+        solutionError = 'Can not querySelectorAll the given solution';
+      }
+    }
+  }
+
+  return { lines, expected, inputError, solutionError, bannedError };
+}
+
+function parseInput(inputValue) {
+  if (!inputValue) {
+    return { input: inputValue, inputError: 'Input can not be empty' };
+  }
+
+  return { input: inputValue, inputError: '' };
+}
+
+function parseSolution(solutionValue) {
+  if (!solutionValue) {
+    return { solution: solutionValue, solutionError: 'Solution can not be empty' };
+  }
+
+  return { solution: solutionValue, solutionError: '' };
+}
+
+function parseBanned(bannedValue) {
+  try {
+    const banned = JSON.parse(bannedValue);
+    if (!Array.isArray(banned)) {
+      return { banned, bannedError: 'Banned characters must be an array. Example: [".", "#", "d"]' };
+    }
+    if (banned.some((char) => !char || typeof char !== 'string')) {
+      return { banned, bannedError: 'Banned characters must be non-empty strings. Example: [".", "#", "d"]' };
+    }
+    return { banned, bannedError: '' };
+  } catch(error) {
+    console.log('Failed to parse `Banned characters`', error);
+    return { banned: [], bannedError: 'Invalid JSON. Example: [".", "#", "d"]' };
+  }
+}
+
+function MarkupRenderer({ lines, expected }) {
   return (
-    <pre>
+    <pre class={style.expected}>
       {lines.map((line, index) => {
-        return <Line line={line} key={index} selected={selected.includes(line.qdId)} />
+        return <Line line={line} key={index} selected={expected.includes(line.qdId)} />
       })}
     </pre>
   );
 }
 
 function Line({ line, selected }) {
+  if (line.tagName === '#text') {
+    return <div class={style.line}>
+      <span>{'  '.repeat(line.indent)}</span>
+      {line.tagValue}
+    </div>;
+  }
+
   return <div class={selected ? style.lineSelected : style.line}>
     <span>{'  '.repeat(line.indent)}</span>
     {
-      line.qdId >= 0
+      line.qdId !== '-1'
       ? <span>
           {'<'}
           <span>{line.tagName}</span>
@@ -94,34 +228,45 @@ function Line({ line, selected }) {
             }
             return <span key={name}>{' '}{parts.join('=')}</span>;
           })}
-          {'>'}
+          {line.selfClosing ? ' />' : '>'}
         </span>
       : <span>{'</'}<span>{line.tagName}</span>{'>'}</span>
     }
   </div>
 }
 
-function nodesToLines(nodes, indent = 0) {
+function nodesToLines(nodes, options, indent) {
   const lines = [];
 
-  nodes.forEach((node, index) => {
-    const qdId = `${indent * 50 + index  }`; // we don't expect more than 50 sibling elements
-    lines.push(nodeToLine(node, qdId, indent));
-    node.setAttribute('data-qdid', qdId);
-    if (node.children.length > 0) {
-      lines.push(...nodesToLines(Array.from(node.children), indent + 1));
+  nodes.forEach((node) => {
+    const qdId = `${++options.qdIdCounter}`;
+    const line = nodeToLine(node, qdId, indent);
+    lines.push(line);
+    if (line.tagName === '#text') {
+      return;
     }
-    lines.push(nodeToLine(node, -1, indent));
+    node.setAttribute('data-qdid', qdId);
+    if (node.children.length > 0 || !line.selfClosing) {
+      lines.push(...nodesToLines(Array.from(node.childNodes), options, indent + 1));
+      lines.push(nodeToLine(node, '-1', indent));
+    }
   });
 
   return lines;
 }
 
+// https://www.w3.org/TR/html51/syntax.html#void-elements
+const VOID_ELEMENTS = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
+
 function nodeToLine(node, qdId, indent) {
+  const tagName = node.nodeName.toLowerCase();
+
   return {
     qdId,
-    tagName: node.tagName.toLowerCase(),
+    tagName,
+    tagValue: node.nodeValue,
+    selfClosing: VOID_ELEMENTS.includes(tagName),
     indent,
-    attributes: Array.from(node.attributes).map(({ name, value }) => ({ name, value })),
+    attributes: Array.from(node.attributes || []).map(({ name, value }) => ({ name, value })),
   };
 }
--- src/components/puzzles/NewJs.js
+++ src/components/puzzles/NewJs.js
@@ -1,5 +1,122 @@
 // YOLO: on
 
-export default function NewJs() {
-  return <div>NEW JS</div>;
+import { h } from 'preact';
+
+import style from './NewJs.css';
+
+export default function NewJs({ state, onChange }) {
+  function onNameChange(event) {
+    const { value } = event.target;
+
+    if (!value) {
+      return onChange({ ...state, name: { value, valid: false, message: 'Name can not be empty' } });
+    }
+
+    onChange({ ...state, name: { value, valid: true } });
+  }
+
+  function onDescriptionChange(event) {
+    onChange({ ...state, description: { value: event.target.value, valid: true } });
+  }
+
+  function onSolutionChange() {
+    const { value } = event.target;
+
+    if (!value) {
+      return onChange({ ...state, solution: { value, valid: false, message: 'Solution can not be empty' } });
+    }
+
+    const { expected, solutionError } = applySolution(state?.input?.value, value);
+
+    return onChange({
+      ...state,
+      expected,
+      solution: { value, valid: !solutionError, message: solutionError },
+    });
+  }
+
+  function onInputChange(event) {
+    const { value } = event.target;
+
+    try {
+      JSON.parse(value);
+      const { expected, inputError } = applySolution(value, state?.solution?.value);
+      return onChange({...state, expected, input: { value, valid: !inputError, message: inputError } });
+    } catch (error) {
+      console.log('Failed to parse `Input`', error);
+      return onChange({...state, input: { value, valid: false, message: 'Can not parse Input' } });
+    }
+  }
+
+  return (
+    <div class={style.panels}>
+      <div class={style.inputs}>
+        <div class={style.title}>Puzzle data</div>
+        <div class={style.name}>
+          <div class={style.propName}>Name:</div>
+          <input class={state?.name?.valid ? style.inputValid : style.inputInvalid} value={state?.name?.value || ''} onInput={onNameChange} />
+          {state?.name?.message && <div class={style.errorMessage}>{state?.name?.message}</div>}
+        </div>
+        <div class={style.description}>
+          <div class={style.propName}>Description:</div>
+          <input value={state?.description?.value || ''} onInput={onDescriptionChange} />
+        </div>
+        <div class={style.solution}>
+          <div><span class={style.propName}>Solution</span> (function body, <span class={style.highlight}>input</span> is passed as <span class={style.highlight}>arg</span> automatically):</div>
+          <textarea class={state?.solution?.valid ? style.inputValid : style.inputInvalid} value={state?.solution?.value || ''} onInput={onSolutionChange} rows="5" cols="50" />
+          {state?.solution?.message && <div class={style.errorMessage}>{state?.solution?.message}</div>}
+        </div>
+        <div class={style.input}>
+          <div><span class={style.propName}>Input</span> (JSON, automatically assigned to <span class={style.highlight}>arg</span> parameter):</div>
+          <textarea class={state?.input?.valid ? style.inputValid : style.inputInvalid} value={state?.input?.value || ''} onInput={onInputChange} rows="5" cols="50" />
+          {state?.input?.message && <div class={style.errorMessage}>{state?.input?.message}</div>}
+        </div>
+        <pre class={style.expected}>
+          {state?.expected}
+        </pre>
+      </div>
+      <div class={style.examples}>
+        <div class={style.title}>Example</div>
+        <div class={style.name}>
+          <div class={style.propName}>Name:</div>
+          <input value="Puzzle name" readonly="readonly" />
+        </div>
+        <div class={style.description}>
+          <div class={style.propName}>Description:</div>
+          <input value="Multiply each element of the array by 2" readonly="readonly" />
+        </div>
+        <div class={style.solution}>
+          <div class={style.propName}>Solution:</div>
+          <textarea value={'return arg.map(e => e * 2);'} readonly="readonly" rows="5" cols="50" />
+        </div>
+        <div class={style.input}>
+          <div class={style.propName}>Input:</div>
+          <textarea value={'[1, 2, 3]'} readonly="readonly" rows="5" cols="50" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function applySolution(input, solution) {
+  let expected = '';
+  let inputError = '';
+  let solutionError = '';
+
+  if (!input) {
+    return { expected, inputError: 'Input can not be empty', solutionError };
+  }
+  if (!solution) {
+    return { expected, inputError, solutionError: 'Solution can not be empty' };
+  }
+
+  try {
+    const stub = {}; // eslint-disable-line no-unused-vars
+    const stubFun = () => {}; // eslint-disable-line no-unused-vars
+    const puzzleContent = JSON.parse(input); // eslint-disable-line no-unused-vars
+    const result = eval(`(function(_, content, ipcRenderer, window, document, location, alert, $, require, process, global, setTimeout, setInterval, requestAnimationFrame, requestIdleCallback, qd) {"use strict"; return (function (arg) {${ solution }})(content);}).call(stub, stub, puzzleContent, stub, stub, stub, stub, stubFun, stub, stubFun, stub, stub, stubFun, stubFun, stubFun, stubFun, stub)`);
+    return { expected: JSON.stringify(result), inputError, solutionError };
+  } catch (error) {
+    return { expected, inputError, solutionError: `Failed to evaluate solution: ${error}` };
+  }
 }
